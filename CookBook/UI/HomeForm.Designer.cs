fd9fa979ef3fa64e99fc2aebc3fa40881285b45d
codebeautyprogramming <codebeautyprogramming@gmail.com>
2024-09-28 01:45:44 +0200

    Startup code for this chapter

---
+++ CookBook/UI/HomeForm.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace CookBook.UI
+{
+    partial class HomeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            FridgeBtn = new Button();
+            RecipesBtn = new Button();
+            FoodManagerBtn = new Button();
+            NotificationIcon = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)NotificationIcon).BeginInit();
+            SuspendLayout();
+            // 
+            // FridgeBtn
+            // 
+            FridgeBtn.Location = new Point(12, 25);
+            FridgeBtn.Name = "FridgeBtn";
+            FridgeBtn.Size = new Size(314, 60);
+            FridgeBtn.TabIndex = 0;
+            FridgeBtn.Text = "Fridge";
+            FridgeBtn.UseVisualStyleBackColor = true;
+            FridgeBtn.Click += FridgeBtn_Click;
+            // 
+            // RecipesBtn
+            // 
+            RecipesBtn.Location = new Point(12, 107);
+            RecipesBtn.Name = "RecipesBtn";
+            RecipesBtn.Size = new Size(314, 60);
+            RecipesBtn.TabIndex = 1;
+            RecipesBtn.Text = "Recipes";
+            RecipesBtn.UseVisualStyleBackColor = true;
+            RecipesBtn.Click += RecipesBtn_Click;
+            // 
+            // FoodManagerBtn
+            // 
+            FoodManagerBtn.Location = new Point(12, 188);
+            FoodManagerBtn.Name = "FoodManagerBtn";
+            FoodManagerBtn.Size = new Size(314, 60);
+            FoodManagerBtn.TabIndex = 2;
+            FoodManagerBtn.Text = "Food manager";
+            FoodManagerBtn.UseVisualStyleBackColor = true;
+            FoodManagerBtn.Click += FoodManagerBtn_Click;
+            // 
+            // NotificationIcon
+            // 
+            NotificationIcon.Image = Properties.Resources.notification;
+            NotificationIcon.Location = new Point(126, 265);
+            NotificationIcon.Name = "NotificationIcon";
+            NotificationIcon.Size = new Size(60, 54);
+            NotificationIcon.SizeMode = PictureBoxSizeMode.StretchImage;
+            NotificationIcon.TabIndex = 3;
+            NotificationIcon.TabStop = false;
+            // 
+            // HomeForm
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(45, 66, 91);
+            ClientSize = new Size(344, 331);
+            Controls.Add(NotificationIcon);
+            Controls.Add(FoodManagerBtn);
+            Controls.Add(RecipesBtn);
+            Controls.Add(FridgeBtn);
+            Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Margin = new Padding(5, 6, 5, 6);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "HomeForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Home";
+            ((System.ComponentModel.ISupportInitialize)NotificationIcon).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button FridgeBtn;
+        private Button RecipesBtn;
+        private Button FoodManagerBtn;
+        private PictureBox NotificationIcon;
+    }
+}
---
+++ CookBook/UI/HomeForm.cs
@@ -0,0 +1,78 @@
+﻿using CookBook.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CookBook.UI
+{
+    public partial class HomeForm : Form
+    {
+        private readonly IServiceProvider _serviceProvider;
+        DesktopFileWatcher _desktopFileWatcher;
+        public HomeForm(IServiceProvider serviceProvider)
+        {
+            InitializeComponent();
+            _serviceProvider = serviceProvider;
+            _desktopFileWatcher= _serviceProvider.GetRequiredService<DesktopFileWatcher>();
+            _desktopFileWatcher.OnFileStatusChanged += OnFileStatusChanged;
+
+            NotificationIcon.Visible = DesktopFileWatcher.CurrentFileStatus;
+        }
+
+        private void OnFileStatusChanged(bool fileExists)
+        {
+            Invoke(new Action(() =>
+            {
+                NotificationIcon.Visible = fileExists;
+            }));
+        }
+
+        private void RecipesBtn_Click(object sender, EventArgs e)
+        {
+            RecipesForm form = _serviceProvider.GetRequiredService<RecipesForm>();
+            ShowForm(form);
+        }
+
+        private void FridgeBtn_Click(object sender, EventArgs e)
+        {
+            IngredientsForm form = _serviceProvider.GetRequiredService<IngredientsForm>();
+            ShowForm(form);
+        }
+
+        private void FoodManagerBtn_Click(object sender, EventArgs e)
+        {
+            //FoodManagerForm form = _serviceProvider.GetRequiredService<FoodManagerForm>();
+            //ShowForm(form);
+
+            ShowForm(_serviceProvider.GetRequiredService<FoodManagerForm>());
+        }
+
+        private void ShowForm(Form form)
+        {
+            //form.StartPosition = FormStartPosition.CenterScreen;
+            form.MaximizeBox = false;
+            form.MinimizeBox = false;
+            form.FormBorderStyle = FormBorderStyle.FixedSingle;
+            form.ShowDialog();
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.S)
+            {
+                SecretForm form = _serviceProvider.GetRequiredService<SecretForm>();
+                ShowForm(form);
+                return true;
+            }
+
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+    }
+}
